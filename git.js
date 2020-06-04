ee220f725eacd872ce9a9f46989fc295150e267f
Milo123459 <milo5huntington@gmail.com>
2020-06-04 15:23:18 +0100

    New update

--- git.js
+++ git.js
@@ -1,2 +1,3 @@
 const cp = require('child_process');
 cp.execSync('git add . | git commit -m "New update" | git push | npm publish')
+//git add . | git commit -m "New update" | git push | npm publish
