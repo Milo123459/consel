95abb435ba2c9a30686280881a3e1b774323b753
Milo123459 <milo5huntington@gmail.com>
2020-06-04 15:31:27 +0100

    New update

--- git.js
+++ git.js
@@ -1,3 +1,1 @@
-const cp = require('child_process');
-cp.execSync('git add . | git commit -m "New update" | git push | npm publish')
-//git add . | git commit -m "New update" | git push | npm publish
+'git add . | git commit -m "New update" | git push | npm publish'
--- install.js
+++ install.js
@@ -2,4 +2,5 @@ const chalk = require("chalk");
 console.log(chalk`
 {green Thanks for using consel.}
 {blue To get started, run consel in the command line.}
+{bold{blue{underline Got a new update? Just run consel -u}}}
 `);
